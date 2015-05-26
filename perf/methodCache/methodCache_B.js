e0f4ed251e9bf2ceeadf868f5ab5d1ff8d9a8c53
Guangyao LIU <guangyao.liu@gree.net>
2015-05-26 13:36:54 +0900

    Added built-in method cache puzzle

---
+++ perf/methodCache/methodCache_B.js
@@ -0,0 +1,21 @@
+function pow_sum(pLength) {
+  var pow = Math.pow;
+  var sum = 0;
+
+  for (var i = 0; i < pLength; i++) {
+    sum += pow(i, 2);
+  }
+
+  return sum;
+}
+
+function measure(pSize) {
+  for (var n = 0; n < pSize; n++) {
+    pow_sum(100);
+  }
+}
+
+var mStart = Date.now();
+measure(10000000);
+var mEnd = Date.now();
+print(mEnd - mStart);
