bec109c4a3f0cd02f5873b3ce8b5235eed878c94
Guangyao LIU <guangyao.liu@gree.net>
2015-05-26 18:59:47 +0900

    Added puzzles demonstrating Chrome DevTool CPU profiler problem of measuring self time

---
+++ tool/selfTime_B.js
@@ -0,0 +1,59 @@
+function MyClass(pA, pB, pC, pD, pE) {
+  this.a = pA;
+  this.b = pB;
+  this.c = pC;
+  this.d = pD;
+  this.e = pE;
+}
+
+function SubClass(pM, pN) {
+  this.bool = pM % 2;
+  this.str = '{' + pN + '}';
+}
+
+
+function childFunc() {
+/* A very important comment explaining how inline works.
+     **************************************************
+     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     --------------------------------------------------
+     An Ascii-art from Wikipedia:
+       |\_/|        ****************************
+      / @ @ \       *  "Purrrfectly pleasant"  *
+     ( > º < )      *       Poppy Prinz        *
+      `»»x««´       *       (pprinz@...)       *
+      /  O  \
+     -------------
+     **************************************************
+     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     --------------------------------------------------
+   */
+  var tArray = [];
+
+  for (var i = 100; i > 0; i--) {
+    tArray[i] = new MyClass(
+      "[" + i + ":" + (i / 3) + "]", 
+      Math.pow(i, 2), 
+      (i * 97 - (i % 2) / 37),
+      Math.max(i, 40, i + 30),
+      new SubClass(i, i / 2)
+    );
+  }
+
+  return tArray;
+}
+
+var mResult = [];
+
+function measure(pPos) {
+  mResult[pPos] = childFunc();
+}
+
+function startTest(pLength) {
+  for (var n = 0; n < pLength; n++) {
+    measure();
+  }
+  
+}
+
+//startTest(100000);
